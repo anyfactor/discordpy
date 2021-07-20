83fb1c8a75d43a9f26c7b64f4eccfed0f3969534
Anyfactor <abdullah.abd.398@gmail.com>
2021-07-21 04:11:01 +0600

    Part 2: Events with member object
    Member join, member remove.

--- bot.py
+++ bot.py
@@ -7,8 +7,19 @@ with open('SECRET', mode='r', encoding='utf-8') as f:
 
 client = commands.Bot(command_prefix = '.')
 
+# the decorator client is referencing the client variable above.
 @client.event
 async def on_ready():
   print('Bot is ready')
+
+@client.event
+async def on_member_join(member):
+# member is a object that can do a lots of things; refer docs
+# this function runs when a member joins the server
+  print(f"{member} has joined the server.")
+
+@client.event
+async def on_member_remove(member):
+  print(f"{member} has left the server")
 
 client.run(SECRET['token'])
