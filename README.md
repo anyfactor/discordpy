11f7ee7f17dee45b01659852932cf04a43b7a833
Anyfactor <abdullah.abd.398@gmail.com>
2021-07-21 12:28:19 +0600

    Guide Finished at Part 11
    There are 3 more videos but I don't need them.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Discordpy Experiments
 
 Experiment series with discordpy.
+
+[Full Guide available here](https://www.notion.so/anyfactor/Part-1-Setup-d58ccbf2bdb8425697162bd039ba6bed)
+
+[Notes where based on Lucas' Playlist.](https://www.youtube.com/playlist?list=PLW3GfRiBCHOhfVoiDZpSz8SM_HybXRPzZ)
--- bot.py
+++ bot.py
@@ -11,19 +11,4 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
   print("Bot is ready")
 
-@client.command()
-@commands.has_permissions(manage_messages=True)
-# Restricts access to the people who can delete and manage messages in a channel
-async def clear(ctx, amount=10):
-  await ctx.channel.purge(limit=amount)
-
-def is_it_me(ctx):
-  return ctx.author.id ==  12351254312344123 # get ID from account
-
-@client.command()
-@commands.check(is_it_me) # custom check function
-# if this is True then the function runs
-async def example(ctx):
-  await ctx.send(f'Hi I am {ctx.author}')
-
 client.run(SECRET['token'])
