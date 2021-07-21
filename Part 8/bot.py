3be1d35067f959ef43c9cf23d6f60c845cb13cfa
Anyfactor <abdullah.abd.398@gmail.com>
2021-07-21 11:32:39 +0600

    Part 9: Background Task
    Background to change status and cycle over a list infinitely.

---
+++ Part 8/bot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from json import load
+
+with open('SECRET', mode='r', encoding='utf-8') as f:
+  SECRET = load(f)
+
+client = commands.Bot(command_prefix = '.')
+
+### Part 8: Bot Status
+@client.event
+async def on_ready():
+  await client.change_presence(status=discord.Status.idle,
+                              activity=discord.Game("Hello There"))
+                              # bot online status
+                              # Bot playing "Hello There"
+  print('Bot is ready')
+
+
+client.run(SECRET['token'])
